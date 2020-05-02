1484512e321b26ddd66b823966a216e594918d9f
Merlin <merlin.seebach@freenet.de>
2020-05-02 17:34:56 +0200

    first try topic2

---
+++ source/topics2.cpp
@@ -0,0 +1,71 @@
+
+#include <bits/stdc++.h>
+
+#define rep(var, bound)   for(int var = 0; var < (bound); ++var)
+#define all(a)      (a).begin(),(a).end()
+#define allStar(a)      (a)->begin(),(a)->end()
+#define endl        '\n'
+
+using namespace std;
+using Graph = vector<vector<int>>;
+using ll = long long;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.precision(10);
+
+
+    //content
+    // input:
+    // n m
+    // m lines with 2 dependencies
+    // output:
+    // 1 line with all the subjects ordered in one possible order
+
+    // ideas:
+    // build dependency graph,
+    // repeatedly take topics that have 0 deps until all are taken
+    //  decrement each topic
+
+    uint n,m,dependent,dependency;
+    cin >> n;
+    cin >> m;
+    auto dependents = new vector<unordered_set<uint>>();
+    auto numberOfDependencies = new vector<uint>();
+    numberOfDependencies->resize(n + 1); // 0 is ignored
+    dependents->resize(n + 1); // 0 is ignored
+
+    auto stack = new vector<uint>();
+
+    for(int i = 0; i < m; i++) {
+        cin >> dependency;
+        cin >> dependent;
+
+        dependents->at(dependency).insert(dependent);
+        numberOfDependencies->at(dependent)++;
+    }
+
+    for(uint i = 1; i <= n; i++) {
+        if(numberOfDependencies->at(i) == 0) {
+            stack->push_back(i);
+        }
+    }
+
+    do {
+        uint elem = stack->at(stack->size()-1);
+        stack->pop_back();
+        cout << elem << " ";
+
+        for_each(all(dependents->at(elem)),[=](uint a){
+            numberOfDependencies->at(a)--;
+            if(numberOfDependencies->at(a) == 0) {
+                stack->push_back(a);
+            }
+        });
+    }while(!stack->empty());
+
+
+    return 0;
+}
+
